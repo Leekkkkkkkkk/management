38541e7854bc4f55eae11cea879b818442b1c176
branches <1425736923@qq.com>
2021-12-12 19:39:04 +0800

    完成了初步清理

---
+++ src/api/user.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+export function login() {
+  return request()
+}
+
+export function getInfo() {
+  return request()
+}
+
+export function logout() {
+  return request()
+}
