61fa7c2bb6908acaaab88d24793a97ab13a817f4
branches <1425736923@qq.com>
2021-12-13 19:31:14 +0800

    完成token过期的处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,14 +1,23 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
 import store from '@/store'
-// import router from '@/router'
-// const timeOut = 2 * 60 * 60 * 1000
+import router from '@/router'
+import { getToken } from './auth'
+const timeOut = 2 * 60 * 60 * 1000
+// const timeOut = 5 * 1000
 const request = axios.create({
   baseURL: 'https://www.liulongbin.top:8888/api/private/v1/'
 })
-// 铺货发送请求
+// 捕获发送请求
 request.interceptors.request.use((config) => {
-  config.headers.Authorization = 'Bearer ' + store.state.user.token
+  if (store.state.user.token) {
+    if (isTimeOut) {
+      store.commit('user/removeUser')
+      router.push('/login')
+      return Promise.reject(new Error('登录已失效'))
+    }
+    config.headers.Authorization = 'Bearer ' + store.state.user.token
+  }
   return config
 })
 // 捕获返回值
@@ -26,5 +35,10 @@ request.interceptors.response.use((res) => {
   Message.error('服务器异常')
   return Promise.reject(err)
 })
-
+function isTimeOut() {
+  const currentTime = Date.now()
+  const tokenTime = getToken()
+  const time = currentTime - tokenTime
+  return time >= timeOut
+}
 export default request
